a30400900c284d01636be7daf0b1c17370142bd1
JouanDeag <61161558+JouanDeag@users.noreply.github.com>
2022-11-12 00:04:07 +0100

    Remove some uneeeded comments

--- main.py
+++ main.py
@@ -21,20 +21,17 @@ def gen():
         pwd += all[number]
         loop += 1
 
-    # wwrite to file
     with open("password.txt", "w") as f:
         f.write(pwd)
 
     return pwd
 
 
-# Check if -p followed by a password argument is passed
 if __name__ == '__main__':
     if (len(sys.argv) == 1):
         main(gen())
     else:
         if (sys.argv[1] == "-s"):
-            # Read from file
             with open("password.txt", "r") as f:
                 main(f.read())
         else:
